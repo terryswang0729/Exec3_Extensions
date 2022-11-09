34581f7eaa5908ed27ffb7e1cbd7c2f8374b2546
士允 王 <terryswang0729@gmail.com>
2022-11-09 15:50:24 +0800

    1109 v1

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -10,6 +10,30 @@ namespace Q2
 	{
 		static void Main(string[] args)
 		{
+			string input = "123456789";
+			string answer = input.Left(3);
+			Console.WriteLine(answer);
+
+			 input = " ";
+			 answer = input.Left(3);
+			Console.WriteLine(answer);
+
+			 input = "1234 ";
+			answer = input.Left(5);
+			Console.WriteLine(answer);
+
+
 		}
 	}
+	public static class StringExtension
+	{
+		public static string Left(this string value,int length)
+		{
+		 if(string.IsNullOrEmpty(value))return string.Empty;
+		 if(length<=0)return string.Empty;
+		 if(value.Length<=length)return value;
+		 return value.Substring(0,length);
+		}
+	} 
 }
+
---
+++ Q3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = "123456";
+			int answer = input.ToInt(0);
+			Console.WriteLine(answer);
+			input = "你好";
+			answer = input.ToInt(0);
+			Console.WriteLine(answer);
+		}
+	}
+	public static class StringExtension
+	{
+		public static int ToInt(this string value, int defaultValue)
+		{
+			bool isInt = int.TryParse(value, out int number);
+			if(isInt)
+			{
+
+				return number;
+			}
+			else
+			{
+				return defaultValue;
+			}
+		}
+	}
+}
