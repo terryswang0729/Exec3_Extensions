7552cc94e5847b6ec7d2406cfb7ba37dcf69e08d
士允 王 <terryswang0729@gmail.com>
2022-11-09 14:47:38 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		  bool result=100.IsOdd();
+			Console.WriteLine(result);
+		  bool result2=100.IsEven();
+			Console.WriteLine(result2);
+		}
+	}
+	public static class IntExtension
+	{
+		public static bool IsOdd(this int source)
+		{
+			return source%2 ==1;
+		}
+		public static bool IsEven(this int source)
+		{
+			return source %2 ==0;
+		}
+	}
+}
